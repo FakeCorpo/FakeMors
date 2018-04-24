b7846481e5c3f639ee4d4dbd5c150dacdd77e4b8
Juan <dawidof11@gmail.com>
2018-04-24 19:33:37 +0200

    Test

--- FakeMors/Program.cs
+++ FakeMors/Program.cs
@@ -17,7 +17,6 @@ namespace FakeMors
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //
         }
     }
 }
