134e4b3ed142feac3d23edef016403b7e8a9afed
Juan <dawidof11@gmail.com>
2018-04-24 19:30:03 +0200

    Commit Test

--- FakeMors/Program.cs
+++ FakeMors/Program.cs
@@ -17,6 +17,7 @@ namespace FakeMors
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //
         }
     }
 }
